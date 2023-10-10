76b233a44f45076ecc66d9dce0ed9ae0f7732082
ArpitBlagan <114584343+ArpitBlagan@users.noreply.github.com>
2023-10-10 17:03:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ useUnifiedTopology: true,
 }).then(con=>{console.log("connnected")});
 const app=express();
 app.use(cors({
-    origin:['http://localhost:5173','https://652534bd209f70165c344158--starlit-seahorse-e2a8d1.netlify.app'],
+    origin:['http://localhost:5173','https://6525362d9f369d16f9b12908--beamish-tiramisu-dce7d4.netlify.app'],
     credentials:true
 }));
 app.use(cookieParser());
@@ -61,7 +61,7 @@ app.get('/google/auth',async(req,res)=>{
          sameSite: 'none',
          secure:true
  }) 
-     res.redirect('https://652534bd209f70165c344158--starlit-seahorse-e2a8d1.netlify.app/main');
+     res.redirect('https://6525362d9f369d16f9b12908--beamish-tiramisu-dce7d4.netlify.app/main');
 });
 app.get('/info',async(req,res)=>{
     const token=req.cookies.token;
@@ -115,7 +115,7 @@ app.get('/logout',async(req,res)=>{
   console.log("req is made on this logout route");
     const token=req.cookies.token;
   if(!token||token==''){
-        return  res.redirect("https://652534bd209f70165c344158--starlit-seahorse-e2a8d1.netlify.app");
+        return  res.redirect("https://6525362d9f369d16f9b12908--beamish-tiramisu-dce7d4.netlify.app");
     }
     console.log(token);
     const val=await tDB.find({access_token:token});
